2c68efe4d33c1f9a08bc7eecc5fa665289284752
wagao <wagao1029@gmail.com>
2019-09-04 12:44:01 +0900

    modify host name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = '<your heroku app>.herokuapp.com'
-  config.action_mailer.default_url_options = { host: "peaceful-inlet-61110.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "https://peaceful-inlet-61110.herokuapp.com" }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
